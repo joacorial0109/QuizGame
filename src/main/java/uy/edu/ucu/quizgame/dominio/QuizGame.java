7759f58a30a769292f6662ec4ebda552799c5dad
joacorial0109 <JOAQUIN.RIAL@correo.ucu.edu.uy>
2026-05-04 19:07:20 -0300

    Corregir integracion de Jugador y QuizGame con TDA

--- src/main/java/uy/edu/ucu/quizgame/dominio/QuizGame.java
+++ src/main/java/uy/edu/ucu/quizgame/dominio/QuizGame.java
@@ -1,12 +1,24 @@
 package uy.edu.ucu.quizgame.dominio;
 
+import uy.edu.ucu.quizgame.tda.TDALista;
+import uy.edu.ucu.quizgame.tda.TDACola;
+import uy.edu.ucu.quizgame.tda.ListaEnlazada;
+import uy.edu.ucu.quizgame.tda.Cola;
+import uy.edu.ucu.quizgame.tda.Pila;
+
 public class QuizGame {
 
     private TDALista<Jugador> jugadores;
     private TDALista<Pregunta> preguntasRegistradas;
-    private TDAColaCircular<Jugador> turnosJugadores;
+    private TDACola<Jugador> turnosJugadores;
     private TDACola<Pregunta> preguntasPendientes;
-    private TDAPila<Respuesta> respuestasRegistradas;
+    private Pila<Respuesta> respuestasRegistradas;
 
-
+    public QuizGame() {
+        this.jugadores = new ListaEnlazada<Jugador>();
+        this.preguntasRegistradas = new ListaEnlazada<Pregunta>();
+        this.turnosJugadores = new Cola<Jugador>();
+        this.preguntasPendientes = new Cola<Pregunta>();
+        this.respuestasRegistradas = new Pila<Respuesta>();
+    }
 }
