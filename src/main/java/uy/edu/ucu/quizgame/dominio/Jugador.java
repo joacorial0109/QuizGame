6a66c135cb038b8e20047fa5db0506189fd71e48
joacorial0109 <JOAQUIN.RIAL@correo.ucu.edu.uy>
2026-05-04 18:49:51 -0300

    Agregar interfaz Lista para TDA

--- src/main/java/uy/edu/ucu/quizgame/dominio/Jugador.java
+++ src/main/java/uy/edu/ucu/quizgame/dominio/Jugador.java
@@ -1,21 +1,89 @@
 package uy.edu.ucu.quizgame.dominio;
 
-public class Jugador {
-    private int id;
-    private String nombre;
-    private int puntaje;
+import uy.edu.ucu.quizgame.tda.Lista;
+import uy.edu.ucu.quizgame.tda.ListaEnlazada;
 
-    public Jugador(int id, String nombre) {
-        this.id = id;
+public class Jugador {
+
+    private int idJugador;
+    private String nombre;
+    private int puntajeActual;
+    private int cantidadCorrectas;
+    private Lista<Respuesta> historialRespuestas;
+
+    public Jugador(int idJugador, String nombre) {
+        this.idJugador = idJugador;
         this.nombre = nombre;
-        this.puntaje = 0;
+        this.puntajeActual = 0;
+        this.cantidadCorrectas = 0;
+        this.historialRespuestas = new ListaEnlazada<Respuesta>();
     }
 
-    public int getId() { return id; }
-    public String getNombre() { return nombre; }
-    public int getPuntaje() { return puntaje; }
+    public int getIdJugador() {
+        return idJugador;
+    }
 
-    public void sumarPuntos(int puntos) {
-        this.puntaje += puntos;
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPuntajeActual() {
+        return puntajeActual;
+    }
+
+    public int getCantidadCorrectas() {
+        return cantidadCorrectas;
+    }
+
+    public void sumarPuntaje(int puntos) {
+        if (puntos > 0) {
+            this.puntajeActual += puntos;
+        }
+    }
+
+    public void restarPuntaje(int puntos) {
+        if (puntos > 0) {
+            this.puntajeActual -= puntos;
+
+            if (this.puntajeActual < 0) {
+                this.puntajeActual = 0;
+            }
+        }
+    }
+
+    public void incrementarCorrectas() {
+        this.cantidadCorrectas++;
+    }
+
+    public void decrementarCorrectas() {
+        if (this.cantidadCorrectas > 0) {
+            this.cantidadCorrectas--;
+        }
+    }
+
+    public void agregarRespuesta(Respuesta respuesta) {
+        if (respuesta != null) {
+            this.historialRespuestas.agregar(respuesta);
+        }
+    }
+
+    public void eliminarUltimaRespuesta() {
+        if (!this.historialRespuestas.estaVacia()) {
+            this.historialRespuestas.eliminarUltimo();
+        }
+    }
+
+    public Lista<Respuesta> getHistorialRespuestas() {
+        return historialRespuestas;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" +
+                "idJugador=" + idJugador +
+                ", nombre='" + nombre + '\'' +
+                ", puntajeActual=" + puntajeActual +
+                ", cantidadCorrectas=" + cantidadCorrectas +
+                '}';
     }
 }
